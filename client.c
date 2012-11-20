e0915db221b648578635b264a14728d9f1a1d659
rucha <rucha@ubuntu.(none)>
2012-11-20 00:49:00 -0500

    client

--- client.c
+++ client.c
@@ -1,3 +1,14 @@
+#include    "unp.h"
+void msg_send( int sockfd_for_write, char *destination_canonical_ip_presentation_format, 
+                int destination_port_number, char *message_to_be_sent, int route_rediscovery_flag );
+int main(int argc, char **argv)
+{
+    int                 sockfd;
+    socklen_t           len;
+    struct sockaddr_un  addr1, addr2;
+
+    if (argc != 2)
+        err_quit("usage: unixbind <pathname>");
 /*
 *    msg_send():    
 *    int          giving the socket descriptor for write
@@ -8,14 +19,7 @@
 *
 */
 
-void msg_send( int sockfd_for_write, char *destination_canonical_ip_presentation_format, 
-                int destination_port_number, char *message_to_be_sent, int route_rediscovery_flag )
-{
-// this  function will write this info in a single char format to sockfd_for_write
-char output_to_sock[MAXLINE];
-output_to_sock= destination_canonical_ip_presentation_format+ "|"+destination_port_number+"|"+message_to_be_sent+"|"+route_rediscovery_flag;
-write(sockfd_for_write,output_to_sock,sizeof(output_to_sock));
-}
+
 
 /*
 When a client is evoked at a node, it creates a domain datagram socket.
@@ -23,13 +27,13 @@ When a client is evoked at a node, it creates a domain datagram socket.
     The client should bind its socket to a ‘temporary’ (i.e., not ‘well-known’) sun_path name obtained from a call to tmpnam() 
     (cf. line 10, Figure 15.6, p. 419) so that multiple clients may run at the same node.
 */
-int fd; 
-char template[] = "fileXXXXXX";
-fd = mkstemp(template);
+    int fd; 
+    char template[] = "fileXXXXXX";
+    fd = mkstemp(template);
 
-bzero(&cliaddr, sizeof(cliaddr));       /* bind an address for us */
-cliaddr.sun_family = AF_LOCAL;
-strcpy(cliaddr.sun_path, template);
+    bzero(&cliaddr, sizeof(cliaddr));       /* bind an address for us */
+    cliaddr.sun_family = AF_LOCAL;
+    strcpy(cliaddr.sun_path, template);
 
 /*
     Note that tmpnam() is actually highly deprecated. You should use the mkstemp() function instead - look up the
@@ -43,7 +47,7 @@ strcpy(cliaddr.sun_path, template);
     the online man pages on minix (‘man unlink’, ‘man remove’) for details. 
 
 
-The client then enters an infinite loop repeating the steps below.
+    The client then enters an infinite loop repeating the steps below.
 
     The client prompts the user to choose one of vm1 , . . . . . , vm10 as a server node.
 
@@ -64,21 +68,45 @@ The client then enters an infinite loop repeating the steps below.
 
     Client repeats steps 1. - 3.
 */
-while(1)
-{
-    char *server_vm;
-    printf("Please select the server VM : vm1,vm2, ... vm10 :\n");
-    scanf("%s",server_vm);
-    printf("Client at node  vm i1  sending request to server at  vm i2\n", );
-
-    msg_send();
-    msg_recv();
-    printf("Client at node  vm i1 : received from   vm i2  <timestamp>\n");
-    if(msg_recv_timeout)
+    while(1)
     {
-        printf("Client at node  vm i1 : timeout on response from   vm i2\n");
-        route_rediscovery_flag=1
-        msg_retransmit();
-    }    
+        char *server_vm;
+        printf("Please select the server VM : vm1,vm2, ... vm10 :\n");
+        scanf("%s",server_vm);
+        printf("Client at node  vm i1  sending request to server at  vm i2\n", );
+
+        msg_send();
+        msg_recv();
+        printf("Client at node  vm i1 : received from   vm i2  <timestamp>\n");
+        if(msg_recv_timeout)
+        {
+            printf("Client at node  vm i1 : timeout on response from   vm i2\n");
+            route_rediscovery_flag=1
+            msg_retransmit();
+        }    
+    }
+
+    sockfd = socket(AF_LOCAL, SOCK_STREAM, 0);
+
+    unlink(argv[1]);        /* OK if this fails */
+
+    bzero(&addr1, sizeof(addr1));
+    addr1.sun_family = AF_LOCAL;
+    strncpy(addr1.sun_path, argv[1], sizeof(addr1.sun_path)-1);
+    Bind(sockfd, (SA *) &addr1, SUN_LEN(&addr1));
+
+    len = sizeof(addr2);
+    Getsockname(sockfd, (SA *) &addr2, &len);
+    printf("bound name = %s, returned len = %d\n", addr2.sun_path, len);
+
+    exit(0);
 }
 
+void msg_send( int sockfd_for_write, char *destination_canonical_ip_presentation_format, 
+                int destination_port_number, char *message_to_be_sent, int route_rediscovery_flag )
+{
+    // this  function will write this info in a single char format to sockfd_for_write
+    char output_to_sock[MAXLINE];
+    output_to_sock= destination_canonical_ip_presentation_format+ "|"+destination_port_number+"|"+message_to_be_sent+"|"+route_rediscovery_flag;
+    write(sockfd_for_write,output_to_sock,sizeof(output_to_sock));
+}
